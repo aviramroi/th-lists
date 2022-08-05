7568035cef119bf63a9efea18be3aa733e53daee
Aviram Roisman <aviram7168@gmail.com>
2022-08-05 18:08:48 +0300

    setup the root _app of the application to wrap everything with: apollo provider, context privider, layout and all the functions of the context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Layout } from "../layout";
+import { useState } from "react";
+import { MainContext } from "../context";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../lib/api";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [favorites, setFavorites] = useState<string[]>([]);
+  const [isFavorite, setFavorite] = useState(false);
+
+  const favoriteToggle = () => setFavorite((prev) => !prev);
+
+  const handleSetFavorites = (id: string) => {
+    if (favorites.includes(id)) {
+      setFavorites((prev) => prev.filter((value) => value != id));
+    } else {
+      setFavorites((prev) => [...prev, id]);
+    }
+  };
+
+  return (
+    <ApolloProvider client={client}>
+      <MainContext.Provider
+        value={{
+          favorites,
+          handleFavoritePressed: handleSetFavorites,
+          isFavorite,
+          viewToggle: favoriteToggle,
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MainContext.Provider>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
